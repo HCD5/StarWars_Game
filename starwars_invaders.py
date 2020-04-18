e4568e10f72753c3c3894ccf72e0ef0316e8471b
HCD5 <howarddickinson@icloud.com>
2020-04-18 19:24:45 -0400

    first commit

---
+++ starwars_invaders.py
@@ -0,0 +1,15 @@
+import sys
+
+import pygame
+
+class StarwarsInvaders:
+    """Main class to manage gamplay"""
+
+    def __init__(self):
+        """Start up game and creat gamplay features"""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200,800))
+        pygame.display.set_caption("StarWars Invaders")
+
+    def run_game(self):
