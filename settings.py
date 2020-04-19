6557b42cd011b047636c5aafbef61b540bdd9371
HCD5 <howarddickinson@icloud.com>
2020-04-18 20:10:48 -0400

    Added settings module

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    "Class to store all of the settings for the game"
+
+    def __init__(self):
+        "Initilize all of the games settings"
+        # Screen settings
+        self.screen_w = 1200
+        self.screen_h = 800
+        self.bg_color = (109, 29, 140)
+        
--- starwars_invaders.py
+++ starwars_invaders.py
@@ -1,6 +1,7 @@
 import sys
-
 import pygame
+
+from settings import Settings
 
 class StarwarsInvaders:
     """Main class to manage gamplay"""
@@ -8,9 +9,11 @@ class StarwarsInvaders:
     def __init__(self):
         """Start up game and creat gamplay features"""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200,800))
+        self.screen = pygame.display.set_mode((self.settings.screen_w,self.settings.screen_h))
         pygame.display.set_caption("StarWars Invaders")
+
 
     def run_game(self):
         """Starts the main loop for gamplay"""
@@ -19,6 +22,9 @@ class StarwarsInvaders:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            # Update backround
+            self.screen.fill(self.settings.bg_color)
 
             # Update screen
             pygame.display.flip()
