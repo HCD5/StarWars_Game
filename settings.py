006d7a8e6d948f7562930be0676b4470f0d56db6
HCD5 <howarddickinson@icloud.com>
2020-04-22 17:10:33 -0400

    Add workspace

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ class Settings:
     """Class to store all of the settings for the game"""
 
     def __init__(self):
-        "Initilize all of the games settings"
+        """Initilize all of the games settings"""
         # Screen settings
         self.screen_w = 1200
         self.screen_h = 800
